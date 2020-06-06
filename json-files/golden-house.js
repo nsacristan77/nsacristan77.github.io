88f1cd974b2c31a9e5de5a13f8c5b988eae4dbc2
Noriel <nlsacristan@gmail.com>
2020-06-07 02:05:26 +0800

    Update golden-house.js

--- json-files/golden-house.js
+++ json-files/golden-house.js
@@ -1,1 +1,21 @@
+const houseData = [
+  {
+    "name": "Manors",
+    "model": "Louisa",
+    "lot": "96 sqm.",
+    "floor": "62 sqm",
+    "bed": "3",
+    "shower": "2",
+    "caport": "1"
+  },
+  {
+    "name": "Manors",
+    "model": "Cecilia",
+    "lot": "96 sqm",
+    "floor": "54 sqm",
+    "bed": "3",
+    "shower": "1",
+    "carport": "1"
+  }
 
+]
