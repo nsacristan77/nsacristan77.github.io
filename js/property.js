291b570459198c9bb1ac5eb8fd1de6800817543f
Noriel <nlsacristan@gmail.com>
2020-06-07 20:50:12 +0800

    update files

---
+++ js/property.js
@@ -0,0 +1,74 @@
+const houseData = [
+  {
+    "name": "Manors",
+    "model": "Louisa",
+    "type": "Single Attached",
+    "price": "3,309,211.00",
+    "lot": "96 sqm.",
+    "floor": "62 sqm.",
+    "bed": "3",
+    "toilet": "2",
+    "carport": "1",
+    "photo": ""
+  },
+  {
+    "name": "Accropolis",
+    "model": "Sofia",
+    "type": "Single Detached",
+    "price": "5,143,978.00",
+    "lot": "125 sqm.",
+    "floor": "110 sqm",
+    "bed": "4",
+    "toilet": "2",
+    "carport": "1"
+    "photo": ""
+  },
+  {
+    "name": "Amara Villas",
+    "model": "Alexandrea",
+    "type": "Single Attached",
+    'price': "2,914,000.00",
+    "lot": "84 sqm.",
+    "floor": "60 sqm.",
+    "bed": "2",
+    "toilet": "2",
+    "carport": "",
+    "photo": ""
+  },
+  {
+    "name": "Via Verde",
+    "model": "Tierra",
+    "type": "Duplex",
+    "price": "2,224,897.00",
+    "lot": "75 sqm.",
+    "floor": "39 sqm",
+    "bed": "2",
+    "toilet": "1",
+    "carport": "",
+    "photo": ""
+  },
+  {
+    "name": "West Governor Heights",
+    "model": "Harriet",
+    "type": "Single Attached",
+    "price": "2,020,000.00",
+    "lot": "75 sqm.",
+    "floor": "65.46 sqm.",
+    "bed": "2",
+    "toilet": "1",
+    "carport": "1",
+    "photo": ""
+  },
+  {
+    "name": "Las Brisas",
+    "model": "Zelia",
+    "type": "Single Attached",
+    "price": "3,483,320.00",
+    "lot": "72 sqm.",
+    "floor": "112.5 sqm.",
+    "bed": "2",
+    "toilet": "2",
+    "carport": "1",
+    "photo": ""
+  }
+]
