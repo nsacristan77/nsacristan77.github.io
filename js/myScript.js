d529af974c88a8264659acc71c3ca085d8ec4fca
Noriel <nlsacristan@gmail.com>
2020-06-11 13:20:45 +0800

    update files

--- js/myScript.js
+++ js/myScript.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
     $(".navbar").slideToggle();
   });
 
-  $('.slider').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
+  $('.golden-slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1
   });
 
 });
