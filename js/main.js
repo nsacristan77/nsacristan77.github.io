51cebb7caaaee2b7012606ff067099f6b5893fd4
greyhawktravelph <greyhawktravelph@gmail.com>
2020-07-01 17:33:30 +0800

    Update files

--- js/main.js
+++ js/main.js
@@ -20,6 +20,29 @@ function openTab(evt, tabName) {
 document.getElementById("defaultOpen").click();
 
 
+
+//JavaScript for dropdown menu
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
+
+//end for javascript dropdown menu
+
+
+
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
